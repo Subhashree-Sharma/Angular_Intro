f2e183c96be011394e27d877cd4c9f30d81f58fb
Subhashree-Sharma <sharmasubhashree88@gmail.com>
2025-03-06 21:37:29 +0530

    [Subhashree Sharma]add.UC_05_DisplayErrorIfWrongInput

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,9 +17,31 @@ export class AppComponent {
   bridgeLabUrl = 'https://www.bridgelabz.com';
   //Uc-04
   userName: string = '';
+  //Uc-05
+  nameError:string ='';
 
   openBridgeLabzSite() {
-    //Opens in new tab
+    //Opens in new tab(Uc-03)
     window.open(this.bridgeLabUrl, '_blank'); 
+  }
+
+  // ngOnInit():void{
+  //   this.title = "Hello, from Bridgelabz";
+  // }
+
+  //Uc-05
+  onClick($event: MouseEvent){
+    console.log("Save button is clicked!", $event);
+    window.open(this.logoUrl, "_blank");
+  }
+
+  onInput(event:Event){
+    console.log("Change Event Occured!",event.target);
+    const nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect!";
   }
 }
